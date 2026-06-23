6ec31ab8e7c2716216ae0d0a51f053984da4d153
Mohammad Razaviyan <mohammadrazaviyan2005@gmail.com>
2026-06-23 04:15:12 +0330

    create menu section

---
+++ components/templates/home/Menu.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import MenuItem from "@/components/modules/MenuItem/MenuItem";
+import Container from "@/components/modules/Container/Container";
+
+export interface Menu {
+    title: string
+    recipe: string
+    price: number
+    url: string
+    off: number
+    id: string
+}
+
+async function Menu() {
+
+    const res = await fetch("http://localhost:8000/menuData")
+    const data = await res.json();
+
+
+    return (
+            <Container className={"mt-40 lg:mt-20 relative"}>
+                <div className={"w-1/2 mx-auto text-center font-[Tanha]"}>
+                    <p className={"text-amber-950 text-[15px] lg:text-[25px] font-bold "}>
+                        از یک ترکیب نوین وو دل انگیز از قهوه لذت ببرید.
+                    </p>
+                    <p className={"text-gray-500 text-[16px] "}>
+                        همه طعم های قهوه را با ما کشف کنید همیشه فنجانی جدید برای تجربه کردن وجود دارد
+                    </p>
+                </div>
+                <div className={"grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 w-[80%] mx-auto my-10"}>
+                    {
+                        data.map((item : Menu ,index :number ) => (
+                            <MenuItem key={index} {...item}/>
+                        ))
+                    }
+                </div>
+            </Container>
+    );
+}
+
+export default Menu;
